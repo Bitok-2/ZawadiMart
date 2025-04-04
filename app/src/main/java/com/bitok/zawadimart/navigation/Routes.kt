3a19ccfdd4b8989e777a74e6e41c78650397a906
Bitok-2 <bitokenock38@gmail.com>
2025-04-04 16:54:30 +0300

    Zawadi

---
+++ app/src/main/java/com/bitok/zawadimart/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.bitok.zawadimart.navigation
+
+const val ROUT_HOME = "home"
+const val ROUT_ABOUT = "about"
+const val ROUT_ITEM = "item"
+const val ROUT_START = "start"
---
+++ app/src/main/java/com/bitok/zawadimart/ui/screens/start/StartScreen.kt
@@ -0,0 +1,96 @@
+package com.bitok.zawadimart.ui.screens.start
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.bitok.zawadimart.R
+import com.bitok.zawadimart.navigation.ROUT_ITEM
+import com.bitok.zawadimart.ui.theme.newOrange
+
+@Composable
+fun StartScreen(navController: NavController){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "ZawadiMart")
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Circular image
+        Image(
+            painter = painterResource(R.drawable.suit),
+            contentDescription = "shopping",
+            modifier = Modifier.size(300.dp).clip(shape = CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        //End
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Text(text = "Find you order!!",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = newOrange
+
+
+
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Has all the information you need on what to buy and where to buy it; ranging from upmarket and modern malls to exemplary artifacts ...\n",
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center
+
+
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Button(
+            onClick = {
+                navController.navigate(ROUT_ITEM)
+            },
+            colors = ButtonDefaults.buttonColors(newOrange),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp)
+        ) {
+            Text(text = "Get Started!")
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartScreenPreview(){
+    StartScreen(rememberNavController())
+
+}
